9aa424d3dff7d0aff749b45e3a3a9ded37a9ee46
Raphaël Peim <raphaelpeimutilitaire@gmail.com>
2020-02-10 10:27:29 +0100

    Create React App

--- projet/src/App.js
+++ projet/src/App.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import './App.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
-
 function App() {
-  const classes = useStyles();
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className={classes.root}>
-          <Button variant="contained">Default</Button>
-          <Button variant="contained" color="primary">
-            Primary
-          </Button>
-          <Button variant="contained" color="secondary">
-            Secondary
-          </Button>
-          <Button variant="contained" disabled>
-            Disabled
-          </Button>
-          <Button variant="contained" color="primary" href="#contained-buttons">
-            Link
-          </Button>
-        </div>
       </header>
     </div>
   );
